c5b46337bc298b31b0b0bd5c11591840b3846931
Mohamad Alshekho <92674451+mohamadmarmara@users.noreply.github.com>
2022-06-11 11:29:41 +0300

    Update database.js

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'ask_experts_anytime',
-    password: 'Mm533487'
+    password: 'database-password'
 })
 
 module.exports = pool.promise();
